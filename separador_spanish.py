92240c6ef4be2faae640daee6d8e7598f7afee98
Zunni97 <115298043+Zunni97@users.noreply.github.com>
2024-06-28 19:20:34 -0700

    Add files via upload

--- separador_spanish.py
+++ separador_spanish.py
@@ -1,13 +1,16 @@
-caracteres_validos = {'a','b','c','d','f','g','h','i','j','k','l',
-                          'm','n','ñ','o','p','q','r','s','t','u','v','w',
-                          'x','y','z','1','2','3','4','5','6','7','8','9',
-                          '0','¡','!','¿','?',':','.',','}
+# Este programa sirve para separar cada letra o caracter del lenguaje español
+# Zunni Rojas Baldivia
+
+caracteres_validos = {'a','á','b','c','d','e','é','f','g','h','i','í','j','k',
+                          'l','m','n','ñ','o','ó','p','q','r','s','t','u','ú',
+                          'ü','v','w','x','y','z','1','2','3','4','5','6','7',
+                          '8','9','0','¡','!','¿','?',':','.',',',' '}
 
 def separar_caracteres(cadena):
     cadena = cadena.lower()
     for caracter in cadena:
         if caracter not in caracteres_validos:
-            return "Error, caracter invalido"
+            return f"Error, caracter invalido {caracter}"
     return list(cadena)
 
 cadena_usuario = input("Ingrese una cadena de texto: ")
